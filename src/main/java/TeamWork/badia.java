be5f41080a7f967eb66a90ed5a882de5007669c7
Badia <b.grioua@gmail.com>
2021-05-25 12:14:14 -0400

    commit by badia

---
+++ src/main/java/TeamWork/badia.java
@@ -0,0 +1,11 @@
+package TeamWork;
+
+public class badia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("code is written by badia");
+
+	}
+
+}
